f63f0de88654bfc31fd9588952dfd19631d720f0
juancinjuarez <juarezjuanmanuel73@gmail.com>
2020-10-18 10:05:50 -0300

    Add files via upload
    Agrego pantalla de carrito

---
+++ Dominio/Carrito.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Carrito
+    {
+        public List<Articulo> listaArticulos { get; set; }
+        public Carrito()
+        {
+            listaArticulos = new List<Articulo>();
+        }
+            
+    }
+}
